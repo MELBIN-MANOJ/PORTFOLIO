dc7395175001af6ab485df95ee8af606e08b26b1
MELBIN-MANOJ <144605779+MELBIN-MANOJ@users.noreply.github.com>
2023-09-10 20:14:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav ul');
 
-});window.addEventListener('DOMContentLoaded', function() {
-    if (window.innerWidth < 768) {
-        // Code for small devices (e.g., mobile phones)
-    } else if (window.innerWidth >= 768 && window.innerWidth < 992) {
-        // Code for tablets
-    } else {
-        // Code for larger screens (e.g., desktops)
-    }
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('show');
 });
